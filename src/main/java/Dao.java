af52c67314064547f73784693350129b8e6b521b
Hannu Mäkilampi <hannu.makilampi@gmail.com>
2022-04-11 10:34:31 +0300

    can select a single candidate by clicking edit and print info to console

--- src/main/java/Dao.java
+++ src/main/java/Dao.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import data.Ehdokas;
 
 public class Dao {
 	
--- src/main/java/EditSingleCandidate.java
+++ src/main/java/EditSingleCandidate.java
@@ -2,9 +2,7 @@
 import data.Ehdokas;
 import java.io.IOException;
 import java.sql.Connection;
-import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class editCandidates
+ * Servlet implementation class EditSingleCandidate
  */
-@WebServlet("/editCandidates")
-public class editCandidates extends HttpServlet {
+@WebServlet("/edit")
+public class EditSingleCandidate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public editCandidates() {
+    public EditSingleCandidate() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,18 +28,12 @@ public class editCandidates extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		Connection con = Dao.createDatabaseConnection(Dao.DBpath, Dao.username, Dao.password);
-		ArrayList<Ehdokas> ehdokkaat = Dao.readFromDatabase(con, Dao.query);
-		request.setAttribute("ehdokasLista", ehdokkaat);
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("./jsp/EditCandidates.jsp");
-		dispatcher.forward(request, response);
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+			
+		int ehdokasId = Integer.parseInt(request.getParameter("id"));
+		Ehdokas ehdokas = Dao.readOneEhdokasFromDatabase(con, Dao.query, ehdokasId);
+		ehdokas.printEhdokas();
 		
 	}
-
-	
 
 }
